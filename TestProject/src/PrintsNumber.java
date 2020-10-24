7e763c7dfa723eb000ef8e8d66222910d77dfbb7
Shweta <Shweta@Shweta-PC>
2020-10-24 21:34:15 +0530

    write code

--- TestProject/src/PrintsNumber.java
+++ TestProject/src/PrintsNumber.java
@@ -3,7 +3,17 @@ public class PrintsNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i=1;
 
+		//print statement
+		System.out.println("Output is : ");
+
+		//loop to print 1 to 10.
+		while(i<=10)	
+		{
+			System.out.println(i);
+			i++;
+		}
 	}
 
 }
