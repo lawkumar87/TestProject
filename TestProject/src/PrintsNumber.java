40d68fc547c657491cab61983804ca17394c030b
lawkumar87 <58648423+lawkumar87@users.noreply.github.com>
2020-10-25 12:01:10 +0530

    Update PrintsNumber.java

--- TestProject/src/PrintsNumber.java
+++ TestProject/src/PrintsNumber.java
@@ -5,10 +5,8 @@ public class PrintsNumber {
 		
 		int i=1;
 
-		//print statement
 		System.out.println("Output is : ");
 
-		//loop to print 1 to 10.
 		while(i<=10)	
 		{
 			System.out.println(i);
